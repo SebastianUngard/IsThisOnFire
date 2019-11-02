5655fea26ad86a4ba0bbcbe39831e35a045b5008
Jason Little <littlj4@rpi.edu>
2019-11-02 19:15:21 -0400

    empty

--- WebApp/main/views.py
+++ WebApp/main/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Image
 from .forms import UploadImageForm
+from .apps import WebappConfig 
+
+
+# Create your views here.
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .apps import WebappConfig
 
 # Create your views here.
 def index(request):
 	return HttpResponse('You are at the index!')
 
-def handle_uploaded_file(f):
-	# process ML model and receive output
-	pass
+
+def popu
+# class call_model(APIView):
+# 	def get(self,request):
+# 	    if request.method == 'GET':
+	        
+# 	        # sentence is the query we want to get the prediction for
+# 	        params =  request.GET.get('sentence')
+	        
+# 	        # predict method used to get the prediction
+# 	        response = WebappConfig.predictor.predict(sentence)
+	        
+# 	        # returning JSON response
+# 	        return JsonResponse(response)
 
 def upload(request):
 	if request.method == 'POST':
