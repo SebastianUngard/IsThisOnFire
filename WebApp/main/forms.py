ee2ad5fe527f0ec8eab067a174232182939e0856
Thomas Hopkins <thomashopkins000@gmail.com>
2019-11-02 21:05:26 -0400

    fixed uploading images

--- WebApp/main/forms.py
+++ WebApp/main/forms.py
@@ -1,5 +1,4 @@
 from django import forms
 
 class UploadImageForm(forms.Form):
-	title = forms.CharField(max_length=50)
 	img = forms.ImageField()
--- WebApp/main/views.py
+++ WebApp/main/views.py
@@ -3,20 +3,27 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Image
 from .forms import UploadImageForm
 
+ANSWER = False
+FORM = None
+
 # Create your views here.
 def index(request):
-	return HttpResponse('Is this on fire?')
+	return render(request, 'main.html')
 
 def handle_uploaded_file(f):
 	# process ML model and receive output
-	pass
+	ANSWER = True
 
 def upload(request):
 	if request.method == 'POST':
-		form = UploadImageForm(title=request.POST, img=request.FILES)
-		if form.is_valid():
+		FORM = UploadImageForm(request.POST, request.FILES)
+		if FORM.is_valid():
 			handle_uploaded_file(request.FILES['img'])
-			return HttpResponseRedirect('/success/url/')
+			return HttpResponseRedirect('/success/')
 	else:
-		form = UploadImageForm()
-	return render(request, 'main.html', {'form': form})
+		FORM = UploadImageForm()
+	return render(request, 'main.html', {'form': FORM})
+
+def output(request):
+	return render(request, 'main.html', {'answer': ANSWER,
+										 'form': FORM})
